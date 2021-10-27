694b06668ceec9ae0bfda25c99a5292d7978b9f8
Maximilian Wernli <maximilian.wernli@ergon.ch>
2021-10-27 08:36:31 +0200

    configure logging with fluentd and elastic search

--- scrapy/run_scrapy.sh
+++ scrapy/run_scrapy.sh
@@ -1,1 +1,1 @@
-docker run --rm "$1" -v "$PWD/digilog":/src --network digilog-data-network scrapy $2
+docker run --rm "$1" -v "$PWD/digilog":/src --network digilog-data-network --log-driver fluentd --log-opt fluentd-address="localhost:24224" --log-opt tag="scrapy" scrapy $2
