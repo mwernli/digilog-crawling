a8f3af14643a76632502a73dc74616a63a11b58c
gerbejon <gerbejon@gmail.com>
2021-10-28 09:00:17 +0200

    additional table in postgres db for storing table with homepages to crawl

---
+++ postgres/flywaymigrations/V0.0002__add_tables.sql
@@ -0,0 +1,10 @@
+CREATE TABLE digilog.local_gov_ch(
+	id		serial	NOT NULL PRIMARY KEY,
+	status		int	NOT NULL,
+	latitude_N	decimal	NOT NULL,
+	latitude_E	decimal	NOT NULL,
+	population	int,
+	elevation	varchar,
+	area		varchar,
+	postalCode	int
+);
