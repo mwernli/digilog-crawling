3d5d500f1a79d96fc36f488db74e4c70bedac5d3
gerbejon <gerbejon@gmail.com>
2021-11-01 10:49:23 +0100

    new flywaymigration

---
+++ postgres/flywaymigrations/V0.0003_add_tables.sql
@@ -0,0 +1,9 @@
+ALTER TABLE digilog.local_gov_ch
+ADD COLUMN gdekt varchar,
+ADD COLUMN gdebznr int,
+ADD COLUMN gdenr int,
+ADD COLUMN gdname varchar,
+ADD COLUMN gdnamk varchar,
+ADD COLUMN gdebzna varchar,
+ADD COLUMN gdektna varchar,
+ADD COLUMN gdemutdat varchar;
