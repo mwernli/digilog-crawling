f1e73bc5021e5cd68d6462c46e6f235583a2de67
gerbejon <gerbejon@gmail.com>
2021-11-02 10:04:29 +0100

    correction name V0.0003

---
+++ postgres/flywaymigrations/V0.0003__add_tables.sql
@@ -0,0 +1,9 @@
+ALTER TABLE digilog.local_gov_ch
+ADD COLUMN gdekt varchar,
+ADD COLUMN gdebznr int,
+ADD COLUMN gdenr int,
+ADD COLUMN gdename varchar,
+ADD COLUMN gdenamk varchar,
+ADD COLUMN gdebzna varchar,
+ADD COLUMN gdektna varchar,
+ADD COLUMN gdemutdat varchar;
