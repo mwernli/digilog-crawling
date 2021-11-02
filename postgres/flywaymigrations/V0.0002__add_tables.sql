166cadcef99d3f11f0a06a16e00708bae75b8710
gerbejon <gerbejon@gmail.com>
2021-11-02 10:22:04 +0100

    correction colname V0.0003

--- postgres/flywaymigrations/V0.0002__add_tables.sql
+++ postgres/flywaymigrations/V0.0002__add_tables.sql
@@ -2,7 +2,7 @@ CREATE TABLE digilog.local_gov_ch(
 	id		serial	NOT NULL PRIMARY KEY,
 	status		int	NOT NULL,
 	latitude_N	decimal	NOT NULL,
-	latitude_E	decimal	NOT NULL,
+	longitude_E	decimal	NOT NULL,
 	population	int,
 	elevation	varchar,
 	area		varchar,
