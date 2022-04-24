f5c89bd5c1a7981ca564dd416fd2da7e0f9611c1
Maximilian Wernli <maximilian.wernli@ergon.ch>
2022-04-24 15:18:16 +0200

    use gunicorn config file

--- web/app/boot.sh
+++ web/app/boot.sh
@@ -3,4 +3,4 @@ source venv/bin/activate
 flask translate compile
 DIGILOG_WEB_SK=$(head -c 60 /dev/random | base64)
 export DIGILOG_WEB_SK
-exec gunicorn -b :5000 --access-logfile - --error-logfile - wsgi:app
+exec gunicorn -b :5000 --config /etc/gunicorn.conf.py wsgi:app
