3d55b9ade0216afbaa2a3352b585b4de7bb23561
gerbejon <48828655+gerbejon@users.noreply.github.com>
2022-05-05 11:39:20 +0200

    change OUTSIDE_NETWORK variable web

--- web/app/wsgi.py
+++ web/app/wsgi.py
@@ -11,5 +11,5 @@ if __name__ == '__main__':
     token = secrets.token_hex()
     os.environ['DIGILOG_WEB_SK'] = token
     sys.path.append(os.path.dirname(__name__))
-    os.environ['OUTSIDE_NETWORK'] = '1'
+    os.environ['OUTSIDE_NETWORK'] = '0'
     app.run(debug=True)
