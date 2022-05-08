92bd7fc91437e8680845ba400eff0f7f7747355e
Maximilian Wernli <maximilian.wernli@ergon.ch>
2022-05-08 22:18:49 +0200

    directly use loki handler in scheduler because sidecars don't work well with kubernetes jobs

--- scheduler/src/logginghelpers.py
+++ scheduler/src/logginghelpers.py
@@ -2,21 +2,40 @@ import logging
 import os
 from logging import Formatter
 
+from datasource import get_env_str_or
+
 
 def get_logging_target():
     try:
         return os.environ['SCHEDULER_LOGGING_TARGET']
     except KeyError:
-        return 'STDOUT'
+        return 'LOKI'
+
+
+def get_loki_handler():
+    from logging_loki import LokiHandler, emitter
+
+    emitter.LokiEmitter.level_tag = 'level'
+
+    loki_host = get_env_str_or('LOKI_SERVICE_HOST', 'localhost')
+    loki_port = get_env_str_or('LOKI_SERVICE_PORT', '3100')
+
+    return LokiHandler(
+        url=f'http://{loki_host}:{loki_port}/loki/api/v1/push',
+        tags={'app': 'digilog-scheduler'},
+        version='1',
+    )
 
 
 def get_handler():
     try:
         target = get_logging_target()
-        if target != 'STDOUT':
-            return logging.FileHandler(filename=target, mode='a')
-        else:
+        if target == 'STDOUT':
             return logging.StreamHandler()
+        elif target == 'LOKI':
+            return get_loki_handler()
+        else:
+            return logging.FileHandler(filename=target, mode='a')
     except FileNotFoundError:
         return logging.StreamHandler()
 
