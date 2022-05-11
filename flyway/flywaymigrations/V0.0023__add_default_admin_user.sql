1ba1c0a118dc4eb3aa6093d11ff1a2a107fd9ba5
Maximilian Wernli <maximilian.wernli@ergon.ch>
2022-05-11 18:13:02 +0200

    add default admin user

---
+++ flyway/flywaymigrations/V0.0023__add_default_admin_user.sql
@@ -0,0 +1,4 @@
+INSERT INTO digilog."user" (username, email, pw_hash)
+VALUES ('admin',
+        'gentle.tea1640@fastmail.com',
+        'pbkdf2:sha256:260000$76ONUr73omDZezYA$2d4da273d84594382a017fb805b8a2dd29901b3a8e0772cc95d53abd4cc39ee0');
