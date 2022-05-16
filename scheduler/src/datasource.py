126f9b82c13c51b742dae80b060900bc806044dd
Maximilian Wernli <maximilian.wernli@ergon.ch>
2022-05-16 17:31:31 +0200

    use named tuples and add mongodb connection

--- scheduler/src/datasource.py
+++ scheduler/src/datasource.py
@@ -4,8 +4,11 @@ import os
 from typing import List, Dict, Optional
 
 import psycopg2
+from bson.objectid import ObjectId
+from psycopg2.extras import NamedTupleCursor
+from pymongo import MongoClient
 
-from model import Municipality
+from model import Municipality, CalibrationRun
 
 logger = logging.getLogger(__name__)
 
@@ -35,9 +38,11 @@ def get_env_int_or(name: str, default: int) -> int:
 class DataSource:
     def __init__(self):
         self.postgres = PostgresConnection()
+        self.mongodb = MongoDbConnection()
 
     def close(self):
         self.postgres.close()
+        self.mongodb.close()
 
 
 class PostgresConnection:
@@ -64,7 +69,8 @@ class PostgresConnection:
             user=self.user,
             password=self.password,
             host=self.host,
-            port=self.port
+            port=self.port,
+            cursor_factory=NamedTupleCursor,
         )
         return connection
 
@@ -103,20 +109,20 @@ class PostgresConnection:
                 """ if uncalibrated_only else ''
                 cursor.execute(
                     f"""
-                    SELECT id, name_de, url FROM municipality m
+                    SELECT id, name_de, url, population, area_sqm FROM municipality m
                     WHERE m.url <> ''
                     {uncalibrated_only}
                     {limit_query}
                     """
                 )
-                return [Municipality(r[0], r[1], r[2]) for r in cursor.fetchall()]
+                return [Municipality.from_named_tuple(r) for r in cursor.fetchall()]
 
     def get_municipality_by_id(self, m_id: int) -> Municipality:
         with self.connection as connection:
             with connection.cursor() as cursor:
                 cursor.execute(
                     """
-                    SELECT id, name_de, url FROM municipality
+                    SELECT id, name_de, url, population, area_sqm FROM municipality
                     WHERE id = %s
                     """,
                     (m_id,)
@@ -124,7 +130,7 @@ class PostgresConnection:
                 result = cursor.fetchone()
                 if not result:
                     raise KeyError(f'Municipality "{m_id}" not found.')
-                return Municipality(result[0], result[1], result[2])
+                return Municipality.from_named_tuple(result)
 
     def schedule_municipality_calibration_runs(self, configuration: Dict[Municipality, dict]):
         with self.connection as connection:
@@ -158,3 +164,73 @@ class PostgresConnection:
                     )
 
                     logger.info(f'scheduled calibration run {queue_id} for {municipality.name_de}')
+
+    def get_finished_calibration_runs(self, limit: Optional[int]) -> List[CalibrationRun]:
+        limit_query = f'LIMIT {limit}' if limit is not None and limit > 0 else ''
+        with self.connection as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    f"""
+                    SELECT
+                    m.id as m_id,
+                    m.name_de,
+                    m.url,
+                    m.population,
+                    m.area_sqm,
+                    cq.id as q_id,
+                    qc.crawl_id,
+                    cs.mongo_stats_id
+                    FROM municipality m
+                    JOIN municipality_to_queue_entry mtqe ON m.id = mtqe.municipality_id
+                    
+                    JOIN crawling_queue cq
+                    ON cq.id = mtqe.queue_id
+                    AND cq.crawl_type = 'calibration'
+                    AND cq.status = 'DONE'
+                    
+                    JOIN queue_crawl qc ON cq.id = qc.queue_id
+                    
+                    JOIN crawl_stats cs ON qc.crawl_id = cs.crawl_id
+                    {limit_query}
+                    """
+                )
+                return [
+                    CalibrationRun(
+                        Municipality(r.m_id, r.name_de, r.url, r.population, r.area_sqm),
+                        r.q_id,
+                        r.crawl_id,
+                        r.mongo_stats_id,
+                    ) for r in cursor.fetchall()]
+
+
+class MongoDbConnection:
+    def __init__(self, called_from_container: bool = False):
+        if called_from_container:
+            self.host = get_env_str_or('MONGODB_SERVICE_HOST', 'digilog-mongodb')
+            self.port = get_env_int_or('MONGODB_SERVICE_PORT', 27017)
+            self.user = get_env_str_or('MONGODB_USER', 'root')
+            self.password = get_env_str_or('MONGODB_PASSWORD', 'mongopwd')
+        else:
+            self.host = 'localhost'
+            self.port = 5550
+            self.user = 'root'
+            self.password = 'mongopwd'
+        self.connection_string = 'mongodb://{}:{}@{}:{}'.format(self.user, self.password, self.host, self.port)
+        self.client = MongoClient(self.connection_string)
+        self.session = self.client.start_session()
+        self.db = self.client.digilog
+
+    def __enter__(self):
+        return self.session.start_transaction()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def close(self):
+        self.session.end_session()
+        self.client.close()
+
+    def get_crawl_stats(self, stats_id: str) -> dict:
+        mongo_stats_id = ObjectId(stats_id)
+        result = self.db.crawlstats.find_one({"_id": mongo_stats_id})
+        return result['stats']
