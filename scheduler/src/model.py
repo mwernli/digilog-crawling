ea997b40be3dade4968816f65b77479be18c253a
Maximilian Wernli <maximilian.wernli@ergon.ch>
2022-05-16 17:31:31 +0200

    use named tuples and add mongodb connection

--- scheduler/src/model.py
+++ scheduler/src/model.py
@@ -1,4 +1,6 @@
+from collections import namedtuple
 from dataclasses import dataclass
+from typing import Optional
 
 
 @dataclass(eq=True, frozen=True)
@@ -6,3 +8,17 @@ class Municipality:
     id: int
     name_de: str
     url: str
+    population: Optional[int]
+    area: Optional[int]
+
+    @staticmethod
+    def from_named_tuple(r: namedtuple):
+        return Municipality(r.id, r.name_de, r.url, r.population, r.area)
+
+
+@dataclass(eq=True, frozen=True)
+class CalibrationRun:
+    municipality: Municipality
+    queue_id: int
+    crawl_id: int
+    stats_id: str
