5761b8fd37f663303b88e432ce2050ee63f2ac01
gerbejon <48828655+gerbejon@users.noreply.github.com>
2022-06-09 14:07:38 +0200

    flyway

--- flyway/flywaymigrations/V0.0027__add_crawl_status_table_for_analysis.sql
+++ flyway/flywaymigrations/V0.0027__add_crawl_status_table_for_analysis.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS digilog.digilog.crawl_run_status
 (
-    crawl_id int NOT NULL REFERENCES digilog.digilog.crawl (id),
-    status   varchar
+    crawl_id int NOT NULL REFERENCES digilog.digilog.municipality (id),
+    queue_id        int NOT NULL REFERENCES digilog.digilog.crawling_queue (id),
+    UNIQUE (municipality_id, queue_id)
 );
