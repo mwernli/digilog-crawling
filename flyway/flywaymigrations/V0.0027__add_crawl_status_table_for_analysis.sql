b71b751bb061c442976a4bdb8c294b9b329c9c6e
Maximilian Wernli <maximilian.wernli@ergon.ch>
2022-06-11 16:57:55 +0200

    delete erroneous migration, and add resolution column to calibration table

--- flyway/flywaymigrations/V0.0027__add_crawl_status_table_for_analysis.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS digilog.digilog.crawl_run_status
-(
-    crawl_id int NOT NULL REFERENCES digilog.digilog.municipality (id),
-    queue_id        int NOT NULL REFERENCES digilog.digilog.crawling_queue (id),
-    UNIQUE (municipality_id, queue_id)
-);
