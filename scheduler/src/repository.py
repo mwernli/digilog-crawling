8a8b440c74dc6669cf7f16d9a431e3cdce633937
Maximilian Wernli <maximilian.wernli@ergon.ch>
2022-06-28 17:33:37 +0200

    rename variable to avoid parameter shadowing

--- scheduler/src/repository.py
+++ scheduler/src/repository.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import logging
 from typing import List, Dict, Optional
@@ -5,7 +6,7 @@ from typing import List, Dict, Optional
 from bson.objectid import ObjectId
 
 from datasource import DataSource
-from model import Municipality, CalibrationRun
+from model import Municipality, CalibrationRun, UrlCheck, UrlCheckResult
 
 logger = logging.getLogger(__name__)
 
@@ -28,7 +29,7 @@ def get_default_settings_by_key(ds: DataSource, settings_key: str) -> dict:
 def municipalities_with_urls(ds: DataSource, limit: Optional[int], uncalibrated_only: bool) -> List[Municipality]:
     with ds.postgres_cursor() as cursor:
         limit_query = f'LIMIT {limit}' if limit is not None and limit > 0 else ''
-        uncalibrated_only = """
+        uncalibrated_only_query = """
             AND recommended_settings IS NULL
             AND NOT EXISTS (
                 SELECT 1 FROM municipality_calibration
@@ -38,8 +39,8 @@ def municipalities_with_urls(ds: DataSource, limit: Optional[int], uncalibrated_
         cursor.execute(
             f"""
             SELECT id, name_de, url, population, area_sqm FROM municipality m
-            WHERE m.url <> ''
-            {uncalibrated_only}
+            WHERE m.url <> '' AND m.do_not_crawl = FALSE
+            {uncalibrated_only_query}
             {limit_query}
             """
         )
@@ -204,7 +205,7 @@ def update_municipality_calibration(ds: DataSource, calibration_id: int, analyse
         cursor.execute(
             """
             UPDATE municipality_calibration 
-            SET analysed = %s, manual_check_required = %s
+            SET analysed = %s, manual_check_required = %s, updated_at = NOW()
             WHERE id = %s
             """,
             (analysed, manual_check_required, calibration_id),
@@ -233,6 +234,16 @@ def set_do_not_crawl(ds: DataSource, municipality_id: int, do_not_crawl: bool):
         )
 
 
+def update_municipality_after_url_check(ds: DataSource, municipality_id: int, url: str, do_not_crawl: bool):
+    with ds.postgres_cursor() as cursor:
+        cursor.execute(
+            """
+            UPDATE municipality SET url = %s, do_not_crawl = %s
+            WHERE id = %s
+            """,
+            (url, do_not_crawl, municipality_id)
+        )
+
 def get_crawl_stats(ds: DataSource, stats_id: str) -> dict:
     mongo_stats_id = ObjectId(stats_id)
     result = ds.mongodb.crawlstats.find_one({"_id": mongo_stats_id})
@@ -245,3 +256,103 @@ def get_multiple_crawl_stats(ds: DataSource, stats_ids: List[str]) -> dict[str, 
     for doc in cursor:
         result[str(doc['_id'])] = doc['stats']
     return result
+
+
+def get_calibrations_with_manual_check_required(ds: DataSource, limit: Optional[int]) -> List[Municipality]:
+    limit_query = f'LIMIT {limit}' if limit is not None and limit > 0 else ''
+    with ds.postgres_cursor() as cursor:
+        cursor.execute(
+            f"""
+            SELECT id, name_de, url, population, area_sqm FROM municipality m
+            WHERE m.url <> ''
+            AND EXISTS(
+                SELECT 1 FROM municipality_calibration mc
+                WHERE mc.municipality_id = m.id
+                AND mc.manual_check_required = TRUE
+                AND mc.resolution IS NULL
+            )
+            {limit_query}
+            """
+        )
+        return [Municipality.from_named_tuple(r) for r in cursor.fetchall()]
+
+
+def update_url_after_manual_check(ds: DataSource, municipality_id: int, new_url: str):
+    with ds.postgres_cursor() as cursor:
+        cursor.execute(
+            """
+            UPDATE municipality
+            SET url = %s, do_not_crawl = FALSE
+            WHERE id = %s
+            """,
+            (new_url, municipality_id,)
+        )
+        cursor.execute(
+            """
+            UPDATE municipality_calibration
+            SET manual_check_required = FALSE, resolution = 'REDIRECT_DETECTED', updated_at = NOW()
+            WHERE municipality_id = %s
+            AND manual_check_required = TRUE
+            AND resolution IS NULL
+            """,
+            (municipality_id,)
+        )
+
+
+def update_manual_calibration_resolution(ds: DataSource, municipality_id: int, resolution: str):
+    with ds.postgres_cursor() as cursor:
+        cursor.execute(
+            """
+            UPDATE municipality_calibration
+            SET resolution = %s, updated_at = NOW()
+            WHERE municipality_id = %s AND manual_check_required = TRUE AND resolution IS NULL
+            """,
+            (resolution, municipality_id,)
+        )
+
+
+def get_urls_to_check(
+    ds: DataSource,
+    limit: Optional[int],
+    not_checked_since_days: int,
+    max_attempts: int,
+) -> List[UrlCheck]:
+    limit_query = f'LIMIT {limit}' if limit is not None and limit > 0 else ''
+    thirty_days_ago = datetime.datetime.today() - datetime.timedelta(days=not_checked_since_days)
+    with ds.postgres_cursor() as cursor:
+        cursor.execute(
+            f"""
+            SELECT m.id, m.url, c.last_check, c.outcome, c.attempts
+            FROM municipality AS m
+            LEFT JOIN url_check c ON c.url = m.url
+            WHERE (last_check IS NULL OR last_check < %s OR (outcome = 'ERROR' AND attempts < %s))
+            AND m.url <> ''
+            AND m.do_not_crawl = FALSE
+            ORDER BY last_check NULLS FIRST 
+            {limit_query}
+            """,
+            (thirty_days_ago, max_attempts),
+        )
+        return [UrlCheck(r.id, r.url, r.last_check, r.outcome, r.attempts) for r in cursor.fetchall()]
+
+
+def update_url_check_result(ds: DataSource, result: UrlCheckResult):
+    with ds.postgres_cursor() as cursor:
+        now = datetime.datetime.utcnow()
+        _update_url_check_result(cursor, result.original_url, now, result.outcome, result.attempts)
+        if result.url_changed():
+            _update_url_check_result(cursor, result.updated_url, now, result.outcome, result.attempts)
+
+
+def _update_url_check_result(cursor, url, last_check, outcome, attempts):
+    cursor.execute(
+        """
+        INSERT INTO url_check (url, last_check, outcome, attempts) 
+        VALUES (%s, %s, %s, %s)
+        ON CONFLICT (url) DO UPDATE SET
+        last_check = excluded.last_check,
+        outcome = excluded.outcome,
+        attempts = excluded.attempts
+        """,
+        (url, last_check, outcome, attempts),
+    )
