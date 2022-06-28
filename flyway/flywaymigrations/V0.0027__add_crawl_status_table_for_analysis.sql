8272ab829117c865a94164a8e0b53ae28085550f
gerbejon <48828655+gerbejon@users.noreply.github.com>
2022-06-28 11:05:28 +0200

    update flyway migration

--- flyway/flywaymigrations/V0.0027__add_crawl_status_table_for_analysis.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS digilog.digilog.crawl_run_status
-(
-    crawl_id int NOT NULL PRIMARY KEY REFERENCES crawl (id) ON DELETE CASCADE,
-    status   varchar,
-    UNIQUE (crawl_id)
-);
