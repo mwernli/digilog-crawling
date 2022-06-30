49b5e194efac15c4800afeff0c57efb7b6d0faaf
gerbejon <48828655+gerbejon@users.noreply.github.com>
2022-06-30 11:15:04 +0200

    change update crawl_status for queued analysis

--- scrapy/digilog/digilog/spiders/simple.py
+++ scrapy/digilog/digilog/spiders/simple.py
@@ -38,7 +38,7 @@ class SimpleSpider(scrapy.Spider):
     def closed(self, reason):
         self.logger.info('Closing spider with reason: "{}"'.format(reason))
         self.save_stats()
-        self.ds.postgres.insert_crawl_status(self.crawl_id, QueueStatus.CRAWLED) if reason == 'finished' else ds.postgres.insert_crawl_status(self.crawl_id, QueueStatus.ERROR)
+        self.ds.postgres.insert_crawl_status(self.crawl_id, 'CRAWLED') if reason == 'finished' else ds.postgres.insert_crawl_status(self.crawl_id, 'ERROR')
         self.ds.close()
 
     def save_stats(self):
